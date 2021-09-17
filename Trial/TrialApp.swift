73c96f97c904cfa5effbba5de93c116df7b231f3
Edu-Sam <edungugi20@gmail.com>
2021-09-17 15:11:37 +0300

    Initial Commit

---
+++ Trial/TrialApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrialApp.swift
+//  Trial
+//
+//  Created by Admin on 17.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct TrialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
