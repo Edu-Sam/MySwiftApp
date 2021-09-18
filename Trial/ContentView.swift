3a57a8abb7c357c07a391dd074a11e88500eb8b6
Edu-Sam <edungugi20@gmail.com>
2021-09-18 11:50:58 +0300

    MapView

--- Trial/ContentView.swift
+++ Trial/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack{
+            
             MapView().frame(height: 300)
             CircleImage().offset(y: -130)
                 .padding(.bottom,-130)
@@ -20,8 +21,18 @@ struct ContentView: View {
                 Spacer()
                     Text("Kenya").font(.subheadline)
                     
-                }
+                }.font(.subheadline).foregroundColor(.secondary)
+                Divider()
+                Text("About Turtle Rock").font(.title2)
+                Text("Your time is limited,so dont waste it living someone elses's life.Dont be trapped by dogma which is the results of other peoples thinking.")
+                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                    .font(.caption2)
+                    .foregroundColor(.gray)
+                    .multilineTextAlignment(.center)
+                    .lineLimit(3)
+                    .truncationMode(.head)
             }.padding()
+            Spacer()
             
         }
     }
